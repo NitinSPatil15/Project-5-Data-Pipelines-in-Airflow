ff135d14290670c89c00fab1ee54521d236313e2
Nitin Shambhurao Patil <p.nitin15@gmail.com>
2020-05-05 18:44:41 +0530

    Updated load fact operator

--- airflow/plugins/operators/load_fact.py
+++ airflow/plugins/operators/load_fact.py
@@ -1,6 +1,8 @@
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+
 
 class LoadFactOperator(BaseOperator):
 
@@ -8,15 +10,24 @@ class LoadFactOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 sql="",
+                 table="",
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id=redshift_conn_id
+        self.aws_credentials_id=aws_credentials_id
+        self.sql=sql
+        self.table=table
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        self.log.info('In load_fact: ')
+        #aws_hook = AwsHook(self.aws_credentials_id)
+        #credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        sql_statement = 'INSERT INTO %s %s' % (self.table, self.sql)
+        redshift.run(sql_statement)
+        self.log.info('load_fact is done')
