156a8c872d9f0456bda7430368b3d5325455deec
Nitin Shambhurao Patil <p.nitin15@gmail.com>
2020-05-05 18:50:51 +0530

    Updated load dimension operator

--- airflow/plugins/operators/load_dimension.py
+++ airflow/plugins/operators/load_dimension.py
@@ -1,6 +1,7 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook
 
 class LoadDimensionOperator(BaseOperator):
 
@@ -8,15 +9,33 @@ class LoadDimensionOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 sql="",
+                 table="",
+                 append_data=None,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id=redshift_conn_id
+        self.aws_credentials_id=aws_credentials_id
+        self.sql=sql
+        self.table=table
+        self.append_data=append_data
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        self.log.info('In load_dimension: ')
+        #aws_hook = AwsHook(self.aws_credentials_id)
+        #credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        if self.append_data == True:
+            sql_statement = 'INSERT INTO %s %s' % (self.table, self.sql)
+            redshift.run(sql_statement)
+        else:
+            sql_statement = 'DELETE FROM %s' % self.table
+            redshift.run(sql_statement)
+            sql_statement = 'INSERT INTO %s %s' % (self.table, self.sql)
+            redshift.run(sql_statement)
+        
+        self.log.info('load_dimension is done')
