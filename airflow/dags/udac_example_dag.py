48b0352c27136abc69e365c4b4da72a51b90b827
Nitin Shambhurao Patil <p.nitin15@gmail.com>
2020-05-05 19:01:22 +0530

    Added create tables operator

--- airflow/dags/udac_example_dag.py
+++ airflow/dags/udac_example_dag.py
@@ -28,6 +28,13 @@ dag = DAG('udac_example_dag',
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
+create_tables = PostgresOperator(
+    task_id="create_tables",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql="create_tables.sql"
+)
+
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
     dag=dag
